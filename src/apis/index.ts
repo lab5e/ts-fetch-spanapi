56f6ddc5b5653e4fa72681f62e56bce81be095b0
Staale Dahl <stalehd@gmail.com>
2021-04-20 15:29:57 +0200

    Initial client version

---
+++ src/apis/index.ts
@@ -0,0 +1,6 @@
+export * from './CollectionsApi';
+export * from './DatadumpApi';
+export * from './DevicesApi';
+export * from './FotaApi';
+export * from './OutputsApi';
+export * from './SystemApi';
